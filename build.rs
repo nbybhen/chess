51c394b4f421f5b37d18be7ccff1f4f9b24acd82
ExplosiveEnd <henockdinberu11@gmail.com>
2024-02-20 20:08:38 -0500

    Update to 0.36.0 for potential bundling patch

--- build.rs
+++ build.rs
@@ -1,3 +1,8 @@
 fn main() {
     println!("cargo:rustc-link-search=framework=/Library/Frameworks");
+    #[cfg(target_os="macos")]
+    println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path");
+
+    #[cfg(target_os="linux")]
+    println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN");
 }
