7cfacc8635bc18f8a01923dd3daf6713faf5c8ff
Henock Dinberu <henockdinberu11@gmail.com>
2024-02-23 03:43:43 -0500

    Fixed 'frameworks doesn't exist on windows' bug

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
 fn main() {
+    #[cfg(target_os="macos")]
     println!("cargo:rustc-link-search=framework=/Library/Frameworks");
+
     #[cfg(target_os="macos")]
     println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path");
 
